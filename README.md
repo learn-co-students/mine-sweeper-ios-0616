c9bfce9f099b61b53b3e309dabe368205f395f36
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-06-10 10:25:19 -0400

    creates tests

---
+++ README.md
@@ -0,0 +1,7 @@
+---
+tags: 
+languages: Ruby
+resources:
+---
+
+## Mine Sweeper
---
+++ spec/mine_sweeper_spec.rb
@@ -0,0 +1,29 @@
+require_relative "spec_helper"
+
+describe "mine_sweeper" do
+  let(:unsweeped_fieled){
+    [
+      [0, 0, 0, 0, '*'],
+      [0, 0, '*', 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, '*', '*', 0, 0],
+      [0, 0, 0, 0, '*']
+    ]
+  }
+  let(:sweeped_field) { 
+    [
+      [0, 1, 1, 1, '*'],
+      [0, 1, '*', 1, 1],
+      [1, 3, 3, 2, 0],
+      [1, '*', '*', 2, 1],
+      [1, 2, 2, 2, '*']
+    ] 
+  }
+
+  it "should increment the spaces" do
+    expect(mine_sweeper(unsweeped_fieled)).to eql(sweeped_field)
+  end
+
+end
+
+
